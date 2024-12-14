3ea86be21d0d8a7696e69bef92f33903837020a8
Vadim Kiryanov <vadim.kiryanov99@mail.ru>
2024-12-14 17:02:55 +0600

    [3] - Рефакторин main.go

--- product-api/main.go
+++ product-api/main.go
@@ -21,28 +21,35 @@ func main() {
 
 	// Конфик сервера
 	s := http.Server{
-		Addr:         ":9090",
-		Handler:      sm,
-		IdleTimeout:  time.Second * 120,
-		ReadTimeout:  time.Second * 1,
-		WriteTimeout: time.Second * 1,
+		Addr:         ":9090",           // configure the bind address
+		Handler:      sm,                // set the default handler
+		ErrorLog:     l,                 // set the logger for the server
+		ReadTimeout:  5 * time.Second,   // max time to read request from the client
+		WriteTimeout: 10 * time.Second,  // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
 	}
 
+	// start the server
 	go func() {
-		// Запускаем сервер на порту 8080
+		l.Println("Starting server on port 9090")
+
 		err := s.ListenAndServe()
 		if err != nil {
-			l.Fatal(err)
+			l.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
 		}
 	}()
 
+	// trap sigterm or interupt and gracefully shutdown the server
 	sigChan := make(chan os.Signal, 1) // Создание каналана
 	signal.Notify(sigChan, os.Interrupt)
 	signal.Notify(sigChan, os.Kill)
 
+	// Block until a signal is received.
 	sig := <-sigChan
 	l.Println("Получено завершение, корректное завершение работы", sig)
 
+	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
 	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
 	s.Shutdown(tc)
 }
