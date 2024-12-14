6863f25628d84884f6e198a429f11f2e7daa255e
Vadim Kiryanov <vadim.kiryanov99@mail.ru>
2024-12-14 17:03:27 +0600

    [4] - Добавление PUT

--- product-api/data/products.go
+++ product-api/data/products.go
@@ -2,34 +2,10 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
-
-type Product struct {
-	ID          int     `json:"_id"`
-	Name        string  `json:"name"`
-	Description string  `json:"description"`
-	Price       float32 `json:"price"`
-	SKU         string  `json:"sku"`
-	CreatedOn   string  `json:"-"`
-	UpdatedOn   string  `json:"-"`
-	DeletedOn   string  `json:"-"`
-}
-type Products []*Product
-
-
-// Добавление к Products метода ToJSON
-func (p *Products) ToJSON(w io.Writer) error {
-	e := json.NewEncoder(w) // Сначала кодируем
-
-	return e.Encode(p) // Потом декодируем
-}
-
-// Получение продуктов
-func GetProducts() Products {
-	return productList
-}
 
 // Моковые данные
 var productList = []*Product{
@@ -51,4 +27,81 @@ var productList = []*Product{
 		CreatedOn:   time.Now().UTC().String(),
 		UpdatedOn:   time.Now().UTC().String(),
 	},
+}
+
+type Product struct {
+	ID          int     `json:"_id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float32 `json:"price"`
+	SKU         string  `json:"sku"`
+	CreatedOn   string  `json:"-"`
+	UpdatedOn   string  `json:"-"`
+	DeletedOn   string  `json:"-"`
+}
+type Products []*Product
+
+// Добавление к Products метода FromJSON
+func (p *Product) FromJSON(r io.Reader) error {
+	e := json.NewDecoder(r) // Сначала кодируем
+
+	return e.Decode(p) // Потом декодируем
+}
+
+// Добавление к Products метода ToJSON
+func (p *Products) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w) // Сначала кодируем
+
+	return e.Encode(p) // Потом декодируем
+}
+
+// UpdateProduct обновляет информацию о продукте с заданным ID.
+// Принимает ID продукта и указатель на объект продукта, который содержит обновленные данные.
+// Возвращает ошибку, если продукт не найден или произошла другая ошибка.
+func UpdateProduct(id int, p *Product) error {
+	// Поиск продукта по ID в списке продуктов
+	var _, f_ID, err = findProduct(id)
+
+	// Если произошла ошибка при поиске, возвращаем её
+	if err != nil {
+		return err
+	}
+
+	// Устанавливаем ID для обновленного продукта
+	p.ID = id
+	// Обновляем продукт в списке продуктов
+	productList[f_ID] = p
+
+	// Возвращаем nil, если обновление прошло успешно
+	return nil
+}
+
+var ErrProductNotFound = fmt.Errorf("Product not found")
+
+func findProduct(id int) (*Product, int, error) {
+	// Итерация и поиск продукта по ид
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+
+	// Если не найден продукт, то nil и ошибка
+	return nil, -1, ErrProductNotFound
+}
+
+// Добавление продукта
+func AddProduct(p *Product) {
+	p.ID = getNexID()
+	productList = append(productList, p)
+}
+
+// Получение продуктов
+func GetProducts() Products {
+	return productList
+}
+
+func getNexID() int {
+	lp := productList[len(productList)-1]
+	return lp.ID + 1
 }
--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
+	"fmt"
 	"log"
 	"main-mode/data"
 	"net/http"
+	"regexp"
+	"strconv"
 )
 
 type Products struct {
@@ -14,19 +17,118 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
+// ServeHTTP обрабатывает входящие HTTP-запросы и перенаправляет их
+// на соответствующие методы обработки в зависимости от HTTP-метода.
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	// Обработка GET-запросов
 	if r.Method == http.MethodGet {
 		p.getProducts(rw, r)
 		return
 	}
 
-	// Отлов всех остальный нереализованных методова
+	// Обработка POST-запросов
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
+
+	// Обработка PUT-запросов
+	if r.Method == http.MethodPut {
+		// Регулярное выражение для извлечения ID из URL
+		reg := regexp.MustCompile(`/([0-9]+)`)
+		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+
+		// Проверка корректности извлеченного ID
+		if len(g) != 1 {
+			p.l.Println("len(g) != 1")
+			http.Error(rw, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+		if len(g[0]) != 2 {
+			p.l.Println("len(g[0]) != 2")
+			http.Error(rw, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+
+		// Преобразование ID из строки в целое число
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			http.Error(rw, "Invalid ID", http.StatusBadRequest)
+			return
+		}
+
+		// Логирование полученного ID
+		fmt.Printf("Got id: %v\n", id)
+		fmt.Printf("g: %v\n", g)
+
+		// Обновление продукта с указанным ID
+		p.updateProducts(id, rw, r)
+		return
+	}
+
+	// Обработка всех остальных не реализованных методов
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
+
+// updateProducts обновляет информацию о продукте с заданным ID.
+// Он принимает ID продукта, а также объекты для записи HTTP-ответа и запроса.
+func (p *Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
+	// Логирование начала обработки PUT-запроса для продуктов
+	p.l.Println("Handle PUT Products")
+
+	// Создание нового экземпляра продукта
+	prod := &data.Product{}
+
+	// Попытка декодирования JSON-данных из тела запроса в объект продукта
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		// Возврат ошибки, если данные не могут быть декодированы
+		http.Error(rw, "Невозможно отправить данные", http.StatusBadRequest)
+		return
+	}
+
+	// Логирование обновленного продукта и его ID
+	p.l.Printf("Prod updated: %#v  idProd: %#v", prod, id)
+
+	// Попытка обновления продукта в хранилище данных
+	var errUpdProd = data.UpdateProduct(id, prod)
+	if errUpdProd == data.ErrProductNotFound {
+		// Возврат ошибки, если продукт с заданным ID не найден
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+
+	if errUpdProd != nil {
+		// Возврат ошибки, если произошла другая ошибка при обновлении продукта
+		http.Error(rw, "Product not found", http.StatusInternalServerError)
+		return
+	}
+}
+
+// Получение продуктов
 func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET Products")
+
 	lp := data.GetProducts()
+
 	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Невозможно получить данные", http.StatusInternalServerError) // Возврат ошибки
 	}
+}
+
+// Добавление продуктов
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Product")
+
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Невозможно отправить данные", http.StatusBadRequest) // Возврат ошибки
+	}
+
+	p.l.Printf("Prod: %#v", prod)
+	data.AddProduct(prod)
 }
